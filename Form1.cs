7e684054d6c32d625d62581ddce6be2c4b239c18
Julio120-max <julsanpal17@hotmail.com>
2025-06-08 00:26:43 -0600

    Casi todo esta listo, faltan pequenas cosas como lo seria un menu para el listbox y tal vez un contextmenustrip, se reparo el codigo. Otra cosa hay que dar pequeños detalles como lo son los signos de colones  o errores ortograficos.

--- Form1.cs
+++ Form1.cs
@@ -5,52 +5,75 @@ using System.Windows.Forms;
 
 namespace Laboratorio_2.git_hub
 {
-
     public partial class Form1 : Form
     {
         string nombre = string.Empty;
         int[] precios = { 5000, 10000, 8000, 4000, 6750 };
         List<int> valores = [];
+        int suma = 0;
+        int residuo = 3000;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        public void validarnombre()
+        // def validar nombre 1.
+        public void nombrevacio()
         {
             if (string.IsNullOrWhiteSpace(txtnombre.Text))
             {
-                lblmensaje.Text = " Por favor, ingrese un nombre.";
+                lblmensaje.Text = " Por favor ingrese un nombre ";
                 lblmensaje.ForeColor = Color.Red;
             }
             else if (ltxservicios.Items.Contains(nombre))
             {
-                lblmensaje.Text = " Ya existe un nombre";
-                lblmensaje.ForeColor = Color.Black;
+                lblmuestra.Text = (" Ya existe un nombre");
+                lblmuestra.ForeColor = Color.Black;
+            }
+        }
+        
+        
+    
+            
+        // def validar nombre.
+        public void validarnombre()
+        {
+            string nombre = txtnombre.Text;
+
+            if (string.IsNullOrWhiteSpace(txtnombre.Text) || ltxservicios.Items.Contains(nombre))
+            {
+                nombrevacio();
             }
             else
             {
                 lblmensaje.Text = " El nombre ha sido guardado correctamente.";
                 lblmensaje.ForeColor = Color.Black;
-                nombre = txtnombre.Text;
                 ltxservicios.Items.Add(nombre);
             }
         }
+        // def validar servicio.
         public void validarservicio()
         {
-            if (string.IsNullOrWhiteSpace(txtnombre.Text))
+            if (string.IsNullOrWhiteSpace(txtnombre.Text) || ltxservicios.Items.Contains(nombre))
             {
-             validarservicio();
-            }
-            else if (cbxseleccionar.SelectedItem == null)
+                nombrevacio();
+            } 
+            if (cbxseleccionar.SelectedItem == null)
             {
-                lblmensaje.Text = "Por favor selecciones un servicio.";
+                lblmensaje.Text = (" Por favor selecciones un servicio.");
                 lblmensaje.ForeColor = Color.Red;
-                ltxservicios.Items.Add(nombre);
-
+                return;
             }
+            else
+            {
+                lblmensaje.Text = " El servicio ha sido agregado correctamente";
+                ltxservicios.Items.Add(cbxseleccionar.SelectedItem);
+            }
+            
+
         }
+        // def agregar precios.
         public void agregarprecios()
         {
             int lugar = cbxseleccionar.SelectedIndex;
@@ -60,68 +83,95 @@ namespace Laboratorio_2.git_hub
             lblmensaje.ForeColor = Color.Black;
             ltxservicios.Items.Add($"{cbxseleccionar.SelectedItem} - ¢{valor}");
         }
+        // def selección de combobox.
         public void seleccioncbx()
         {
-            if (string.IsNullOrWhiteSpace(txtnombre.Text))
+            if (string.IsNullOrWhiteSpace(txtnombre.Text) || ltxservicios.Items.Contains(nombre))
             {
-             validarnombre();
+                nombrevacio();
             }
             else if (cbxseleccionar.SelectedItem == null)
             {
                 validarservicio();
             }
-           else
-           {
-             agregarprecios();
-           }
+            else
+            {
+                agregarprecios();
+            }
+            
         }
+        // def salir de todo.
         public void salirdetodo()
         {
             this.Close();
         }
 
-
-        private void btnsalir_Click(object sender, EventArgs e)
-        {
-            salirdetodo();
-        }
-
-        private void btncalculo_Click(object sender, EventArgs e)
-        {
-            CalcularTotal();
-        }
-
-        public void CalcularTotal()
-        {
-         validarservicio();
-
-            int suma = 0;
-            foreach (var valores in valores)
-            {
-                suma += valores;
-                lblresultado.Text = $"{suma}";
-
-                if (suma > 35000)
-
-                    lblresultado.Text = "Total elevado revise su seleccion: ";
-
-                else
-                    suma -= 3000;
-                lblresultado.Text = "Total con descuento: " + suma;
-            }
-        }
-
-        public void btnlimpiar_Click(object sender, EventArgs e)
+        // def limpiar.
+        public void limpiar()
         {
             txtnombre.Clear();
             ltxservicios.Items.Clear();
-            lblresultado.Text = "";
+            lblresultado.Text = " Resultado: ";
+        }
 
+
+
+        public void CalcularTotal()
+        {
+            if (string.IsNullOrWhiteSpace(txtnombre.Text) || ltxservicios.Items.Contains(nombre))
+            {
+                nombrevacio();
+            }
+            else if (cbxseleccionar.SelectedItem == null)
+            {
+                validarservicio();
+            }
+            else
+            {
+                
+                foreach (var valor in valores)
+                {
+                    suma += valor;
+                }
+
+                
+                int candtidaddeservicios = ltxservicios.Items.Count;
+
+                lblresultado1.Text = $"Total descuentos: ¢{3000} : Sub total: ¢{suma} :";
+
+                if (candtidaddeservicios > 3)
+                {
+                    suma -= 3000;
+                    lblmuestra.Text = " Se ha aplicado el descuento.";
+                    lblresultado.Text = $"\nTotal con descuento aplicado: ¢{suma}. ";
+                }
+                else if ( suma > 35000)
+                {
+                    lblmensaje.Text = "Total elevado revise su seleccion: ";
+                }
+            }
+        }
+
+        //Botones
+        private void btnnombre_Click(object sender, EventArgs e)
+        {
+            validarnombre();
         }
         private void btnagregar_Click(object sender, EventArgs e)
         {
             seleccioncbx();
-
+        }
+        private void btncalculo_Click(object sender, EventArgs e)
+        {
+            CalcularTotal();
+        }
+        public void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            salirdetodo();
         }
     }
 }
